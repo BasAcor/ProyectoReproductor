e18806b4dffca5be4d2914b487c481f0ed561dc3
Regi-Ch <regi_ch6@hotmail.com>
2024-04-15 04:47:51 -0600

    Sidebar

--- proyectoremu/src/components/Sidebar.jsx
+++ proyectoremu/src/components/Sidebar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {IoLibrary} from 'react-icons/io5';
-import {MdHomeFilled, MdSearch} from 'react-icons/md';
+import { IoLibrary } from 'react-icons/io5';
+import { MdHomeFilled, MdSearch } from 'react-icons/md';
+import Playlists from './Playlists';
 export default function Sidebar() {
-  return  (<Container>Sidebar
+  return  (<Container>
     <div className="top__links">
         <div className="logo">
             <img 
             src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
             alt="remu"
             />
-        </div>
-    </div>
-    <div>   
+        </div>   
         <ul>
             <li>
                 <MdHomeFilled/>
@@ -28,11 +27,44 @@ export default function Sidebar() {
             </li>
         </ul>
     </div>
+    <Playlists/>
 </Container>);
 }
 
 const Container = styled.div`
     background-color: black;
-
+    color:#b3b3b3;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    .top__links {
+        display: flex;
+        flex-direction: column;
+        .logo {
+            text-align: center;
+            margin: 1rem 0;
+            img {
+                max-inline-size: 80%;
+                block-size: auto;
+            }
+        }
+        ul {
+            list-style-type: none;
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            padding: 1rem;
+            li {
+                display: flex;
+                gap: 1rem;
+                cursor: pointer;
+                transition: 0.3s ease-in-out;
+                &:hover {
+                    color: white;
+                }
+            }
+        }
+    }
 `;
 
